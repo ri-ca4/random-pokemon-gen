67c1c8dd96adb144ff013b969f89e4c84502755d
ri-ca4 <riley.de12@gmail.com>
2022-12-18 16:17:34 -0500

    display data

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,17 +1,35 @@
-const infoDiv = document.getElementById('info');
-const genBtn = document.getElementById('gen');
+const genBtn    = document.getElementById('gen');
+
+const nameDiv   = document.getElementById('name');
+const levelDiv  = document.getElementById('level');
+const typeDiv   = document.getElementById('type');
+const imageDiv  = document.getElementById('image');
+const lengthDiv = document.getElementById('length');
+const weightDiv = document.getElementById('weight');
+
+const moveDiv  = document.getElementById('move');
+
 
 const generateCard = ()=>{
-    var pkmn = Math.floor(Math.random() * 1010);
+    var pkmn = Math.floor(Math.random() * 151);
+    var lvl = Math.floor(Math.random() * 100);
+    levelDiv.innerHTML = lvl
 
     fetch(`https://pokeapi.co/api/v2/pokemon/${pkmn}`)
     .then((response) => response.json())
-    .then((data) => 
-        console.log(data)
-        
-        
-        
-    );
+    .then((data) => {
+        let types = data.types
+        let moves = data.moves
+        var moveNum = Math.floor(Math.random() * moves.length)
+        console.log(moves[moveNum].move.name)
+
+        nameDiv.innerHTML = data.name
+        typeDiv.innerHTML = types[0].type.name
+        imageDiv.innerHTML = `<img src='${data.sprites.front_default}' alt="${data.name}"/>`
+        lengthDiv.innerHTML = data.height
+        weightDiv.innerHTML = data.weight
+        moveDiv.innerHTML = moves[moveNum].move.name
+    });
 
 }
 
