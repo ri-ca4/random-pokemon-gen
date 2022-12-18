4008e3da0c41a6fe16af1997f33f3e3907ee18c8
ri-ca4 <riley.de12@gmail.com>
2022-12-18 16:46:44 -0500

    convert height and weight, add descriptive text to numbers

--- js/pokemon.js
+++ js/pokemon.js
@@ -13,7 +13,7 @@ const moveDiv  = document.getElementById('move');
 const generateCard = ()=>{
     var pkmn = Math.floor(Math.random() * 151);
     var lvl = Math.floor(Math.random() * 100);
-    levelDiv.innerHTML = lvl
+    levelDiv.innerHTML = 'lvl ' + lvl
 
     fetch(`https://pokeapi.co/api/v2/pokemon/${pkmn}`)
     .then((response) => response.json())
@@ -21,13 +21,19 @@ const generateCard = ()=>{
         let types = data.types
         let moves = data.moves
         var moveNum = Math.floor(Math.random() * moves.length)
-        console.log(moves[moveNum].move.name)
+
+        var height = data.height * 3.937 //convert decimeters to inches
+        var heightIn = Math.floor(height % 12) //remaining inches after convert to feet
+        var heightFt = Math.floor(height / 12) //convert to feet
+
+        var weight =  Math.floor(data.weight / 4.536) //convert hectograms to lbs
+
 
         nameDiv.innerHTML = data.name
         typeDiv.innerHTML = types[0].type.name
         imageDiv.innerHTML = `<img src='${data.sprites.front_default}' alt="${data.name}"/>`
-        lengthDiv.innerHTML = data.height
-        weightDiv.innerHTML = data.weight
+        lengthDiv.innerHTML = heightFt + "'" + heightIn
+        weightDiv.innerHTML = weight + ' lbs'
         moveDiv.innerHTML = moves[moveNum].move.name
     });
 
