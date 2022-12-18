6385d437a0a8585c48d05aaab44d217b26e1bad7
ri-ca4 <riley.de12@gmail.com>
2022-12-18 15:05:47 -0500

    add fetch

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,1 +1,18 @@
-alert('connected')
+const infoDiv = document.getElementById('info');
+const genBtn = document.getElementById('gen');
+
+const generateCard = ()=>{
+    var pkmn = Math.floor(Math.random() * 1010);
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pkmn}`)
+    .then((response) => response.json())
+    .then((data) => 
+        console.log(data)
+        
+        
+        
+    );
+
+}
+
+genBtn.addEventListener('click', generateCard)
